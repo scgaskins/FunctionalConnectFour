1cc14cd646d59563e3bf8bf9b45487aee5a5f78e
Garrett <colliergc@hendrix.edu>
2022-04-22 21:58:56 -0500

    Main notes/comments for goal of project within Connect4 module

--- Connect4.hs
+++ Connect4.hs
@@ -1,3 +1,14 @@
-Connect4.hs
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
 
---run the connect4 game (main function)
+-- main: make board and call event loop
+
+-- event loop:
+
+-- draw board
+-- is it won?
+-- print the win message
+-- not won?
+-- get a move
+-- recursive call to event loop with updated board, next player
