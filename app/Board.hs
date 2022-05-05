8c7dd305f97609c28d166f0175c4a81970f4599d
Garrett <colliergc@hendrix.edu>
2022-05-05 13:05:40 -0500

    Format Board

--- app/Board.hs
+++ app/Board.hs
@@ -23,6 +23,10 @@ type Board = [[Color]]
 emptyBoard :: Int -> Int -> Board
 emptyBoard columns rows = replicate columns (replicate rows Empty)
 
+--formatBoard gives the board new line characters to split the board by rows
+--to read the board easier for the users
+formatBoard :: Board -> String
+formatBoard = unlines . map show
 
 -- Returns Columns whose topmost row is still not filled
 possibleMoves :: Board -> [Column]
--- app/Main.hs
+++ app/Main.hs
@@ -17,7 +17,7 @@ main = do
   putStrLn "four in a row before the other player." 
   putStrLn "The board is 7 columns x 6 rows"
   putStrLn "Here is the starting board!"
-  print startingBoard --print initial board to screen
+  putStrLn (formatBoard startingBoard) --print initial board to screen
   --play game with starting board and first player Red
   playGame startingBoard 0
     where startingBoard = emptyBoard 6 7
@@ -27,7 +27,7 @@ playGame board playerIndex
   | checkWin Red board    = putStrLn "Red Wins!!" 
   | checkWin Yellow board = putStrLn "Yellow Wins!!"
   | otherwise = do
-      putStrLn ("Player " ++ show playerIndex ++ "(" ++ show (changePlayers players playerIndex) ++ "):")
+      putStrLn ("Player " ++ show playerIndex ++ " (" ++ show (changePlayers players playerIndex) ++ "):")
       putStrLn "Enter a column number "
       putStrLn "from 0 to 5 to place your piece: "
       playerInput <- getLine
@@ -35,21 +35,8 @@ playGame board playerIndex
       --update board with piece
       case makeMove (changePlayers players playerIndex) playerColumnNum board of
         Just newBoard -> do
-          print newBoard
+          putStrLn (formatBoard newBoard)
           playGame newBoard (update playerIndex)
         Nothing       -> do
           putStrLn "That is not a valid column"
           playGame board playerIndex
-      -- print board
-      -- playGame board 
-      --   where newBoard = 
-  
-
--- event loop:
-
--- draw board
--- is it won? (done)
--- print the win message
--- not won? (done)
--- get a move (done)
--- recursive call to event loop with updated board
